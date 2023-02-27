e04f7e7c67f0b26d27cb90afd06dedcd967bc39e
Brian <bbarr0101@gmail.com>
2023-02-27 11:48:14 -0800

    adds the main menu prompt

--- chat.c
+++ chat.c
@@ -7,7 +7,7 @@ int port;
 
 int main(int argc, char const *argv[])
 {
-    
+    char cmd[16]; 
     /* handle port assignment from cmdline */
     if (argc < 2)
     {
@@ -16,12 +16,44 @@ int main(int argc, char const *argv[])
     {
         port = atoi(argv[1]); 
     }
-    
 
-    printf("%d", port);
+    /* create thread for server/socket on the machine to listen */
 
+
+    while (1)
+    {
+        printf(">: ");
+        scanf("%s", cmd);
+        printf("cmd is: %s \n", cmd);
+        printf("cmd[0] = %c \n", cmd[0]);
     
-    //GetMyIp();
+    //strip the first letter from the cmd input and use it for the switch
+    switch (cmd[0])
+    {
+    case 'h':
+        printf("here is a list of commands and their usage: \n");
+        printf("------------------------------------------- \n");
+        printf("help: displays this menu\n");
+        printf("ip: shows the users ip address \n");
+        printf("port: \n");
+        printf("list: list available concections on the p2p system \n");
+        printf("connect: \n");
+        printf("terminate: \n");
+        printf("send: \n");
+        printf("exit \n");
+        break;
+    case 'm':
+        printf("your ip: \n");
+    case 'e':
+        exit(0);
+    default:
+        printf("input not unsderstood\n");
+        break;
+    }
+    }
+
+
+  
     return 0;
 }
 
