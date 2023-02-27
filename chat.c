f07e0c79ec79e04d584a4fcc16782e49b8c7d130
Brian <bbarr0101@gmail.com>
2023-02-27 10:54:42 -0800

    handles incoming port argt from commandline

--- chat.c
+++ chat.c
@@ -1,12 +1,27 @@
-
 #include "server.h"
-
+#include <stdlib.h>
 #define SERVER_ADDR "172.217.160.99"
 #define SERVER_PORT 80
 
-int main()
-{
-    GetMyIp();
+int port; 
 
+int main(int argc, char const *argv[])
+{
+    
+    /* handle port assignment from cmdline */
+    if (argc < 2)
+    {
+        port = 8080;
+    }else
+    {
+        port = atoi(argv[1]); 
+    }
+    
+
+    printf("%d", port);
+
+    
+    //GetMyIp();
     return 0;
 }
+
