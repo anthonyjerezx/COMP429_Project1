565d11f3b438f3d6009297ac20420767982298b4
Brian <bbarr0101@gmail.com>
2023-02-27 12:23:10 -0800

    implements main menu switch

--- chat.c
+++ chat.c
@@ -3,57 +3,69 @@
 #define SERVER_ADDR "172.217.160.99"
 #define SERVER_PORT 80
 
-int port; 
+int port;
 
 int main(int argc, char const *argv[])
 {
-    char cmd[16]; 
+    char cmd[16];
     /* handle port assignment from cmdline */
     if (argc < 2)
     {
         port = 8080;
-    }else
+    }
+    else
     {
-        port = atoi(argv[1]); 
+        port = atoi(argv[1]);
     }
 
     /* create thread for server/socket on the machine to listen */
-
 
     while (1)
     {
         printf(">: ");
         scanf("%s", cmd);
-        printf("cmd is: %s \n", cmd);
-        printf("cmd[0] = %c \n", cmd[0]);
-    
-    //strip the first letter from the cmd input and use it for the switch
-    switch (cmd[0])
-    {
-    case 'h':
-        printf("here is a list of commands and their usage: \n");
-        printf("------------------------------------------- \n");
-        printf("help: displays this menu\n");
-        printf("ip: shows the users ip address \n");
-        printf("port: \n");
-        printf("list: list available concections on the p2p system \n");
-        printf("connect: \n");
-        printf("terminate: \n");
-        printf("send: \n");
-        printf("exit \n");
-        break;
-    case 'm':
-        printf("your ip: \n");
-    case 'e':
-        exit(0);
-    default:
-        printf("input not unsderstood\n");
-        break;
-    }
+
+/*      printf("cmd is: %s \n", cmd);
+        printf("cmd[0] = %c \n", cmd[0]); */
+
+        // strip the first letter from the cmd input and use it for the switch
+        switch (cmd[0])
+        {
+        case 'h':
+            printf("here is a list of commands and their usage \n");
+            printf("------------------------------------------- \n");
+            printf("help: displays this menu\n");
+            printf("ip: shows the users ip address \n");
+            printf("port: shows the current port number\n");
+            printf("list: list available concections on the p2p system \n");
+            printf("connect: \n");
+            printf("terminate: \n");
+            printf("send: \n");
+            printf("exit \n");
+            break;
+        case 'i':
+            GetMyIp();
+            break;
+        case 'p':
+            printf("you're port#: %d \n",port);
+            break;
+        case 'l':
+            printf("list \n");
+            break;
+        case'c':
+            printf("connect\n"); 
+            break;
+        case't':
+            printf("Terminate \n");
+        case 's':
+            printf("send\n");
+        case 'e':
+            exit(0);
+        default:
+            printf("input not unsderstood\n");
+            break;
+        }
     }
 
-
-  
     return 0;
 }
-
