27328906d6d5b5602c2c47b0a1b3e6472799d54b
Brian <bbarr0101@gmail.com>
2023-03-02 08:49:42 -0800

    server work in progress

--- chat.c
+++ chat.c
@@ -4,18 +4,52 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#define SERVER_ADDR "172.217.160.99"
-#define SERVER_PORT 80
+#include<netinet/in.h>
+
 
 int port;
+char cmd[16];
+struct sockaddr_in cli_addr;
+int sock;
+struct sockaddr_in self_addr;
+struct sockaddr_in peer_list[100];
 
-void *server_func(void *arg){
-    printf("server func ran \n");
+void print_help();
+
+void *server_func(void *arg)
+{
+    //printf("server init \n");
+    struct sockaddr_in serv_addr;
+    int socketfd;
+    if (socketfd = socket(AF_INET, SOCK_STREAM, 0) < 0)
+    {
+        perror("failed to init");
+    }
+    //printf("socket successfully made\n");
+    
+    //flushes out the server address
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+    while (1)
+    {
+        sleep(5);
+        printf("server running in the background\n");
+    }
+    
 }
 
 int main(int argc, char const *argv[])
 {
-    char cmd[16];
+
+
+    pthread_t server_thread;
+    int iret;
+    char message[255];
+
+
     /* handle port assignment from cmdline */
     if (argc < 2)
     {
@@ -26,59 +60,51 @@ int main(int argc, char const *argv[])
         port = atoi(argv[1]);
     }
 
+
     /* create thread for server/socket on the machine to listen */
-    pthread_t server_thread; 
-    int iret = pthread_create(&server_thread, NULL, server_func, NULL);
-    printf("%d \n", iret);
-
-
-    // loop for the client 
-    char message[255];
+    
+   
+    if (iret = pthread_create(&server_thread, NULL, server_func, NULL) != 0)
+    {
+        perror("thread failed to start \n");
+    }
+    printf("thread succesfully made \n");
+    pthread_join(server_thread, NULL);
+     // print_help();
+    // loop for the client
+    
     while (1)
     {
         printf(">: ");
-        fgets(cmd,16,stdin);
-
-/*      printf("cmd is: %s \n", cmd);
-        printf("cmd[0] = %c \n", cmd[0]); */
+        fgets(cmd, 16, stdin);
 
         // strip the first letter from the cmd input and use it for the switch
         switch (cmd[0])
         {
         case 'h':
-            printf("here is a list of commands and their usage \n");
-            printf("------------------------------------------- \n");
-            printf("help: displays this menu\n");
-            printf("ip: shows the users ip address \n");
-            printf("port: shows the current port number\n");
-            printf("list: list available concections on the p2p system \n");
-            printf("connect: \n");
-            printf("terminate: \n");
-            printf("send: \n");
-            printf("exit: leave the program \n");
-            printf("\n\n");
+            print_help();
             break;
         case 'i':
             GetMyIp();
             break;
         case 'p':
-            printf("you're port#: %d \n",port);
+            printf("you're port#: %d \n", port);
             break;
         case 'l':
             printf("list \n");
             break;
-        case'c':
-            printf("connect\n"); 
+        case 'c':
+            printf("connect\n");
             break;
-        case't':
+        case 't':
             printf("Terminate \n");
             break;
         case 's':
             printf("enter your message: ");
-            //clear out the message every time
+            // clear out the message every time
             memset(message, 0, 255);
             fgets(message, 255, stdin);
-            printf("your message: %s \n ",message);
+            printf("your message: %s \n ", message);
             break;
         case 'e':
             exit(0);
@@ -89,4 +115,19 @@ int main(int argc, char const *argv[])
     }
 
     return 0;
+}
+
+void print_help()
+{
+    printf("here is a list of commands and their usage \n");
+    printf("------------------------------------------- \n");
+    printf("help: displays this menu\n");
+    printf("ip: shows the users ip address \n");
+    printf("port: shows the current port number\n");
+    printf("list: list available concections on the p2p system \n");
+    printf("connect: \n");
+    printf("terminate: \n");
+    printf("send: \n");
+    printf("exit: leave the program \n");
+    printf("\n\n");
 }
