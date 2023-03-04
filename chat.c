185913b9af2c58ed44c743c9be0a882881e68ab8
blbarr01 <bbarr0101@gmail.com>
2023-03-04 11:02:42 -0800

    creates new function to request a conection to a peer. used for the connection case

--- chat.c
+++ chat.c
@@ -4,55 +4,66 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include<netinet/in.h>
-
+#include <netinet/in.h>
 
 int port;
 char cmd[16];
 int sock;
 int clientfds[10];
 
-struct sockaddr_in mySockaddr; 
+struct sockaddr_in mySockaddr;
 pthread_mutex_t io_lock, stdin_lock;
 
 void print_help();
 
-void *server_func(void *arg){
-    int *server_fd = (int*)arg;
+void *server_func(void *arg)
+{
+    int *server_fd = (int *)arg;
     int peer_fd;
     socklen_t addr_size;
-    struct sockaddr_storage their_addr; 
+    struct sockaddr_storage their_addr;
     char pip[32];
 
     while (1)
     {
-        if(listen(*server_fd,10)<0){
+        if (listen(*server_fd, 10) < 0)
+        {
             perror("falied to listen");
             pthread_cancel(pthread_self());
         }
 
-        addr_size = sizeof their_addr; 
-        if(peer_fd = accept(*server_fd, (struct sockaddr*)&their_addr, (unsigned int*)sizeof&addr_size) <0);
-            perror("failed to accept");
-        inet_ntop(AF_INET,&their_addr,pip,32); 
-
-
-
+        addr_size = sizeof their_addr;
+        if (peer_fd = accept(*server_fd, (struct sockaddr *)&their_addr, (unsigned int *)sizeof &addr_size) < 0)
+            ;
+        perror("failed to accept");
+        inet_ntop(AF_INET, &their_addr, pip, 32);
     }
+}
+
+req_connect()
+{
+    // request connection ip
+    char rcip[32];
+    // request connection port number
+    int rcpn;
+    printf("connect\n");
+    printf("enter the ip : ");
+    fgets(rcip, 32, stdin);
+    printf("enter the port you wish to coonect on: ");
+    char temp[32];
+    fgets(temp, 32, stdin);
+    rcpn = atoi(temp);
+    printf("connect call args: %s %d", rcip, rcpn);
 }
 
 int main(int argc, char const *argv[])
 {
 
-
     pthread_t server_thread;
     int iret;
     char message[255];
     int my_fd;
-    //request connection ip 
-    char rcip[32];
-    //request connection port number
-    int rcpn;
+
     /* handle port assignment from cmdline */
     if (argc < 2)
     {
@@ -63,21 +74,21 @@ int main(int argc, char const *argv[])
         port = atoi(argv[1]);
     }
 
-    // create a socket to listen on 
-    my_fd = socket(AF_INET,SOCK_STREAM,0);
-    if(my_fd < 0)
+    // create a socket to listen on
+    my_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (my_fd < 0)
         perror("failed to make a socket");
 
-    mySockaddr.sin_family = AF_INET; 
-    mySockaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    mySockaddr.sin_port = port; 
+    mySockaddr.sin_family = AF_INET;
+    mySockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    mySockaddr.sin_port = port;
     printf("socket generated \n");
     /* create thread for server/socket on the machine to listen */
-    if(bind(my_fd, (struct sockaddr*)&mySockaddr, sizeof(mySockaddr))<0)
+    if (bind(my_fd, (struct sockaddr *)&mySockaddr, sizeof(mySockaddr)) < 0)
         perror("failed to bind");
-    
-    pthread_create(&server_thread, NULL, server_func, (void *)&my_fd); 
-    
+
+    pthread_create(&server_thread, NULL, server_func, (void *)&my_fd);
+
     while (1)
     {
         print_help();
@@ -99,11 +110,7 @@ int main(int argc, char const *argv[])
             printf("list \n");
             break;
         case 'c':
-            printf("connect\n");
-            printf("enter the ip : ");
-            fgets(rcip, 32,stdin);
-            printf("\nenter the port you wish to coonect on: ");
-            fgets(rcpn, 32,stdin);
+            req_connect();
             break;
         case 't':
             printf("Terminate \n");
